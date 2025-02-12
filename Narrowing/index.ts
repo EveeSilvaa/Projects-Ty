159f5b3b0d76d2d17588ff86bb191962dd1874d3
eveesilvaa <evellynsilvadeveloper@gmail.com>
2025-02-11 23:28:05 -0300

    new configs

---
+++ Narrowing/index.ts
@@ -0,0 +1,15 @@
+// 1 - type guard
+function sum(a: number | string, b: number | string) {
+
+    if(typeof a === "string" && typeof b === "string") {
+        console.log(parseFloat(a) + parseFloat(b))
+    } else if (typeof a === "number"&& typeof b === "number" ) {
+        console.log(a + b)
+    } else {
+        console.log("impossível realizar a soma!") // exemplo de caso para o terceiro caso retornar inavalido
+    }
+}
+
+sum("4", "59") //exemplo com numero de string
+sum(12, 42.3) // emplo com number 
+sum("4", 6) // exemplo com string e number
