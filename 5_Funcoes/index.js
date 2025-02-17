ed6a1ad245e44d99083ed235aa47f05bb3213b7b
eveesilvaa <evellynsilvadeveloper@gmail.com>
2025-02-17 09:55:01 -0300

    NEW

--- 5_Funcoes/index.js
+++ 5_Funcoes/index.js
@@ -15,7 +15,7 @@ function preGreeting(f, userName) {
 }
 preGreeting(greeting, "maria");
 preGreeting(greeting, "kaio");
-// generics functions
+// 3 - generics functions
 function firstElement(arr) {
     return arr[0];
 }
@@ -29,3 +29,22 @@ function mergeObjects(obj1, obj2) {
 }
 const newObject = mergeObjects({ name: "maria" }, { age: 21, job: "programmer" });
 console.log(newObject);
+// 4 - constraints nas generics function 
+function biggestNumber(a, b) {
+    let biggest;
+    if (+a > +b) {
+        biggest = a;
+    }
+    else {
+        biggest = b;
+    }
+    return biggest;
+}
+console.log(biggestNumber(4, 7));
+console.log(biggestNumber("12", "5"));
+// 5 -  especificar tipo de argumento
+function mergeArrays(arr1, arr3) {
+    return arr1.concat(arr3);
+}
+console.log(mergeArrays([1, 2, 3], [5, 6]));
+console.log(mergeArrays([1, 2, 3], ["teste", "testando"]));
--- 5_Funcoes/index.ts
+++ 5_Funcoes/index.ts
@@ -21,7 +21,7 @@ function preGreeting(f: (name: string) => string, userName: string) {
 preGreeting(greeting, "maria")
 preGreeting(greeting, "kaio")
 
-// generics functions
+// 3 - generics functions
 function firstElement<T>(arr: T[]): T {
     return arr[0]
 }
@@ -38,3 +38,27 @@ function mergeObjects<U, T> (obj1: U, obj2: T) { //funcao que une obejetos
 const newObject = mergeObjects({name: "maria"}, {age: 21, job: "programmer"})
 
 console.log(newObject)
+
+// 4 - constraints nas generics function 
+function biggestNumber<T extends number | string>(a: T, b: T): T {
+    let biggest: T
+
+    if(+a > +b) {
+        biggest = a
+    } else {
+        biggest = b
+    }
+
+    return biggest
+}
+
+console.log(biggestNumber(4, 7))
+console.log(biggestNumber("12", "5"))
+
+// 5 -  especificar tipo de argumento
+function mergeArrays<T>(arr1: T[], arr3: T[]) {
+    return arr1.concat(arr3)
+}
+
+console.log(mergeArrays([1, 2, 3], [5, 6]))
+console.log(mergeArrays<number | string>([1, 2, 3], ["teste", "testando"]))
