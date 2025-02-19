2861aab5c722eb568cd179f238258ab50391bf4d
evellyn <maevee38@gmail.com>
2025-02-19 16:53:56 -0300

    new commit

--- 6_ObjectType/index.js
+++ 6_ObjectType/index.js
@@ -1,1 +1,14 @@
 "use strict";
+function showProductDetails1(product) {
+    console.log(`O nomedo produto é ${product.name} e seu preço é  ${product.price}`);
+    if (product.isAvailable) {
+        console.log('Este produto está disponível');
+    }
+}
+const shirt1 = {
+    name: "camisa polo",
+    price: 39.90,
+    isAvailable: true
+};
+showProductDetails1(shirt1);
+showProductDetails1({ name: "camisa polo", price: 39.90, isAvailable: false });
--- 6_ObjectType/index.ts
+++ 6_ObjectType/index.ts
@@ -4,7 +4,7 @@ interface Product {
     price: number
     isAvailable: boolean
 }
-function showProductDetails(product: Product) {
+function showProductDetails1(product: Product) {
     console.log(`O nomedo produto é ${product.name} e seu preço é  ${product.price}`)
     if(product.isAvailable) {
         console.log('Este produto está disponível')
@@ -16,5 +16,5 @@ const shirt1:Product = {
     price: 39.90,
     isAvailable: true 
 }
-showProductDetails(shirt1)
-showProductDetails({name: "camisa polo", price: 39.90, isAvailable: false})
+showProductDetails1(shirt1)
+showProductDetails1({name: "camisa polo", price: 39.90, isAvailable: false})
