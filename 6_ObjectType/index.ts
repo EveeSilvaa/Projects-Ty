e47d3b49b65c856df42bd66c4f4f3984a5403cb6
evellyn <maevee38@gmail.com>
2025-02-19 16:33:24 -0300

    new commit

--- 6_ObjectType/index.ts
+++ 6_ObjectType/index.ts
@@ -2,9 +2,19 @@
 interface Product {
     name: string
     price: number
-    isAvailable: true
+    isAvailable: boolean
 }
-function showProductDetails(name: string, price: number, isAvailable: true): Product {
-    return { name, price, isAvailable }
+function showProductDetails(product: Product) {
+    console.log(`O nomedo produto é ${product.name} e seu preço é  ${product.price}`)
+    if(product.isAvailable) {
+        console.log('Este produto está disponível')
+    }
 }
-    
+
+const shirt1:Product = {
+    name:"camisa polo",
+    price: 39.90,
+    isAvailable: true 
+}
+showProductDetails(shirt1)
+showProductDetails({name: "camisa polo", price: 39.90, isAvailable: false})
