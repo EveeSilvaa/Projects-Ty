af9b011315cd141889091d17aa1d9f2674726346
eveesilvaa <evellynsilvadeveloper@gmail.com>
2025-02-19 22:27:57 -0300

    estudando

--- 6_ObjectType/index.ts
+++ 6_ObjectType/index.ts
@@ -20,7 +20,45 @@ showProductDetails1(shirt1)
 showProductDetails1({name: "camisa polo", price: 39.90, isAvailable: false})
 
 // 2 - inteface com parametro opcional
+interface User {
+    email: string, 
+    role?: string
+}
+
+function showUserDetails(user: User) {
+    console.log(`O email do usuário é: ${user.email}`)
+    
+    if(user.role) {
+        console.log(`E seu papel é: ${user.role}`)
+    }
+}
+const u1:User = {
+    email: "evesilva.contato@gmail.com", role: "admin",
+    name: ""
+}
+const u2:User = {
+    email: "evellynsilva@gmail.com",
+    name: ""
+}
+
+showUserDetails(u1)
+showUserDetails(u2)
+
 // 3 - propriedades readonly
+interface Car {
+    brand: string,
+    readonly wheels: number
+}
+
+const fusca: Car = {
+    brand: "Ford",
+    wheels: 4
+}
+console.log(fusca)
+
+
+
+
 // 4 - index signature
 // 5 - herança de interface
 // 6 - intersection type
