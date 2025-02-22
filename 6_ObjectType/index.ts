1cfe5b6f3e171a8b768cbecedaf449abaab74f5e
eveesilvaa <evellynsilvadeveloper@gmail.com>
2025-02-22 08:00:00 -0300

    finalizacao de object types

--- 6_ObjectType/index.ts
+++ 6_ObjectType/index.ts
@@ -57,13 +57,98 @@ const fusca: Car = {
 console.log(fusca)
 
 
-
-
 // 4 - index signature
+interface CoordObject {
+    [index: string]: number
+}
+
+let coords : CoordObject = {
+    x: 10
+}
+coords.y = 20
+
+console.log(coords)
 // 5 - herança de interface
-// 6 - intersection type
+interface Human {
+    name: string
+    age: number
+}
+interface SuperHuman extends Human {
+    superPower: string[]
+}
+
+const mario: Human = {
+    name: "Maria",
+    age: 25
+}
+console.log(mario)
+
+const goku: SuperHuman = {
+    name: "Goku",
+    age: 35,
+    superPower: ["Super Sayajin", "Kamehameha"]
+}
+console.log(goku)
+console.log(goku.superPower[1])
+// 6 - intersection types
+interface Character {
+    name: string
+}
+
+
+interface Gun {
+    type: string
+    caliber: number
+}
+
+type humanWithGun = Human & Gun 
+
+const arnold: humanWithGun = {
+    name: "Arnold",
+    age: 70,
+    type: "Desert Eagle",
+    caliber: 50
+}
+console.log(arnold)
+console.log(arnold.name)
+console.log(arnold.caliber)
+
 // 7 - readonly array
+let myArray: ReadonlyArray<string> = ["maçã", "Laranja", "Banana"]
+console.log(myArray)
+
+myArray.forEach((item) =>{
+    console.log("Fruta: " + item)
+})
+
+myArray = myArray.map((item) => {
+    return `Fruta: ${item}`
+})
+console.log(myArray)
+
 // 8 - tuplas
+type fiveNumbers = [number, number, number, number, number]
+
+const myNumberArray: fiveNumbers = [1, 2, 3, 4, 5]
+// const myNumberArray2: fiveNumbers = [1, 2, 3, 4, 5, 6] // nao posso adicionar mais array que utrapassem 5
+// const mixedArray: fiveNumbers = [1, true, "teste", 4, 5]
+
+console.log(myNumberArray)
+
+type nameAndAge = [string, number]
+
+const anotherUser:nameAndAge = ["Eve", 25]
+console.log(anotherUser[0])
+
+anotherUser[0] = "Evellyn"
+console.log(anotherUser[0])
+
 // 9 - tuplas com readonly
+function showNumbers(number: readonly [number, number]) {
+    console.log(number[0])
+    console.log(number[1])
+}
+
+showNumbers([1, 2])
 
 
