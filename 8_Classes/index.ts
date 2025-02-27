ac949d14833e5a5744be58ed3edbfefd62228f8c
eveesilvaa <evellynsilvadeveloper@gmail.com>
2025-02-27 08:08:25 -0300

    New Commit

---
+++ 8_Classes/index.ts
@@ -0,0 +1,15 @@
+// campos com classe 
+class user {
+    name! : string
+    age ! : number
+}
+
+const mavie = new user()
+
+console.log(mavie)
+
+mavie.name = "mavie"
+
+console.log(mavie)
+
+// 
