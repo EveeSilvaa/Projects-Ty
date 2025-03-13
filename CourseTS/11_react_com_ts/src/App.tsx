48c6caa5206259851d072713c64918ec1dcff701
eveesilvaa <evellynsilvadeveloper@gmail.com>
2025-03-13 14:50:48 -0300

    Feat: Finalização da Seção11.

--- CourseTS/11_react_com_ts/src/App.tsx
+++ CourseTS/11_react_com_ts/src/App.tsx
@@ -1,10 +1,31 @@
-// 4 - desestruturando props
-import SecondComponent from "./components/SecondComponent";
-import Destructuring from "./components/Destructuring";
+import React, {createContext} from "react";
 
 // 3 - importação de components
 import FirstComponet from "./components/FirstComponet";
 
+// 4 - desestruturando props
+import SecondComponent from "./components/SecondComponent";
+import Destructuring, { Category } from "./components/Destructuring";
+
+// 5 - useState
+import State from "./components/State";
+
+// 7 - usando o context
+import Context from "./components/Context";
+
+// 6 - type
+type textOrNull = string | null
+
+//type fixed = "isso" | "ou" | "aquilo";
+
+// 7 -  context
+interface IAppContext {
+  language: string;
+  framework: string;
+  projects: number;
+}
+
+export const AppContext = createContext<IAppContext | null> (null)
 
 function App() {
   // 1 - Variaveis
@@ -17,8 +38,24 @@ function App() {
     return `Olá, ${name}!`;
   };
 
+  // 6 - type
+  const myText: textOrNull = "Tem algum texto aqui";
+  let mySecondText: textOrNull = null;
+
+  //mySecondText = "opa";
+
+  //const testandoFixed: fixed = "isso";
+
+  // 7 - context api
+  const contextValue = {
+    language: "javascript",
+    framework: "Express",
+    projects: 5,
+  };
+
 
   return (
+    <AppContext.Provider value={contextValue}>
     <div className="App">
       <h1>Typescript com React</h1>
       <h2>Nome: {name}</h2>
@@ -34,8 +71,14 @@ function App() {
        content="Algum conteudo"
        commentsQty={10}
        tags={["ts", "js"]}
+       category={Category.TS}
        />
+      <State />
+      {myText && <p>Tem texto na variavel</p>}
+      {mySecondText && <p>Tem texto na variavel</p>}
+      <Context />
     </div>
+  </AppContext.Provider>
   );
 }
 
