cc690aced87b3c3ecf30ef6df2639589963790ab
eveesilvaa <evellynsilvadeveloper@gmail.com>
2025-03-12 23:57:50 -0300

    Feat: Aprimorando estudos

--- CourseTS/11_react_com_ts/src/App.tsx
+++ CourseTS/11_react_com_ts/src/App.tsx
@@ -1,10 +1,29 @@
+// 3 - importação de components
+import FirstComponet from "./components/FirstComponet";
 
-import './App.css';
 
 function App() {
+  // 1 - Variaveis
+  const name: string = "viviane";
+  const age: number = 17;
+  const isWorking: boolean = true;
+
+  // 2 - Funções
+  const userGreeting = (name: string): string => {
+    return `Olá, ${name}!`;
+  };
+
+
   return (
     <div className="App">
       <h1>Typescript com React</h1>
+      <h2>Nome: {name}</h2>
+      <p>Idade: {age}</p>
+      {isWorking && ( //estamos fazendo uma condicional com o booleano e concatenando para retornar
+        <p>Está Trabalhando!</p>
+       )} 
+       <h3>{userGreeting(name)}</h3>
+       <FirstComponet />
     </div>
   );
 }
---
+++ CourseTS/11_react_com_ts/src/components/FirstComponet.tsx
@@ -0,0 +1,14 @@
+import React, { ReactElement } from "react";
+
+
+
+
+function FirstComponet(): ReactElement {
+  return (
+    <div>
+        <h1>Meu primeiro component</h1>
+    </div>
+  )
+}
+
+export default FirstComponet;
